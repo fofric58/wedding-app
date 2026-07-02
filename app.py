17aa04b26c89db00c93e5d05267b6cd3ec07b59e
fofric58 <fofricmorgan997@gmail.com>
2026-07-02 14:43:12 +0300

    Add zipfile import for file handling

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import zipfile
+from flask import send_file
 import os
 from flask import Flask, render_template, request, redirect, session, send_from_directory
 from werkzeug.utils import secure_filename
